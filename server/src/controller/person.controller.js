094c383e35c10ad985699884ef8bcea4a12785cb
William Le <115980802+WilliamAtSebratec@users.noreply.github.com>
2022-12-17 21:57:30 +0100

    create react-app

--- server/src/controller/person.controller.js
+++ server/src/controller/person.controller.js
@@ -1,5 +1,5 @@
-import responseHandler from "../handlers/response.handler";
-import tmdbApi from "../tmdb/tmdb.api";
+import responseHandler from "../handlers/response.handler.js";
+import tmdbApi from "../tmdb/tmdb.api.js";
 
 const personDetails = async (req, res) => {
     try {
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,13 +2,15 @@ import express from "express"
 import userRoute from "./user.route.js"
 import reviewRoute from "./review.route.js"
 import mediaRoute from "./media.route.js"
-
-
+import personRoute from "./person.route.js"
 
 
 const router = express.Router()
 
 router.use("/user",userRoute)
+router.use("/person",personRoute)
+router.use("/review",reviewRoute)
+router.use("/:mediaType",mediaRoute)
 
 export default router;
 
