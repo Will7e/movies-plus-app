b1f849421f4394b34392c25d703edad96e7658ff
William Le <william7e.se@gmail.com>
2023-01-02 21:39:03 +0100

    update baseurl

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:8000/api/v1/";
+const baseURL = "https://moviesplus-api.onrender.com/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
